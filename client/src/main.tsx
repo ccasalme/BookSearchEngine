571ddad3247ff6e85111c5e226198a6bc31b96e1
ccasalme <78170262+ccasalme@users.noreply.github.com>
2025-03-06 17:17:05 -0500

    apollo

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+import client from './apolloClient.js'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from './App.jsx'
-import SearchBooks from './pages/SearchBooks'
-import SavedBooks from './pages/SavedBooks'
+import App from './App.jsx';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
 
 const router = createBrowserRouter([
   {
@@ -15,14 +17,17 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <SearchBooks />
-      }, {
+      },
+      {
         path: '/saved',
         element: <SavedBooks />
       }
     ]
   }
-])
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
-)
+  <ApolloProvider client={client}>  {/* ✅ Wrap in ApolloProvider */}
+    <RouterProvider router={router} />
+  </ApolloProvider>
+);
